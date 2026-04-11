f85430cd5b47d0fe5e12b619138522fd52cbfd78
Vincent Anuwat Van Duin <vivan5333@uit.no>
2026-04-11 17:25:51 +0200

    core physics added

--- src/game_logic/physics.py
+++ src/game_logic/physics.py
@@ -1,5 +1,33 @@
+import pygame
+#temp const
+dt = 0.01
 
+class Core_physics(pygame.sprite.Sprite):
 
+    
+
+    def __init__(self, x, y):
+        super().__init__()
+        self.position = [x, y]
+        self.velocity = [0.0, 0.0]
+        self.acceleration = [0.0, 0.0] 
+        self.angle = 0.0
+
+    def apply_physics(self):
+        # update velocity
+        self.velocity[0] += self.acceleration[0] * dt
+        self.velocity[1] += self.acceleration[1] * dt
+
+        #max velocity
+        self.velocity[0] = max(-5, min(5, self.velocity[0]))
+        self.velocity[1] = max(-5, min(5, self.velocity[1]))
+
+        # update position
+        self.position[0] += self.velocity[0] * dt
+        self.position[1] += self.velocity[1] * dt
+
+    def gravity(self):
+        self.acceleration[1] += 9.81 * dt
 
 class physics_engine:
     def __init__(self):
